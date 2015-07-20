b20300966b087c7958f2f532aa83e243ff6b27fa
Luke Jones <luke@freerunningtechnologies.com>
2015-07-20 10:23:03 -0700

    Add mailer gem and config

--- config/settings.rb
+++ config/settings.rb
@@ -1,8 +1,9 @@
 class Settings
-  attr_reader :harvest
+  attr_reader :harvest, :mailer
 
   def initialize(path)
     @harvest = Settings::Harvest.new
+    @mailer = Settings::Mailer.new
 
     hash = YAML::load_file(path)
     hash.each do |settings_key, settings_value|
@@ -16,5 +17,9 @@ class Settings
 
   class Harvest
     attr_accessor :username, :password, :subdomain
+  end
+
+  class Mailer
+    attr_accessor :smtp, :smtp_port, :from_name, :from_email, :domain, :username, :password
   end
 end
--- models/application.rb
+++ models/application.rb
@@ -5,6 +5,16 @@ class Application
     @settings = settings
     Log.verbose = true
     Log.file = 'log.txt'
+    Mail.defaults do
+      delivery_method :smtp,
+        address: settings.mailer.smtp,
+        port: settings.mailer.smtp_port,
+        domain: settings.mailer.domain,
+        user_name: settings.mailer.username,
+        password: settings.mailer.password,
+        authentication: 'plain',
+        enable_starttls_auto: true
+    end
   end
 
   def shame_all
