a7800cbbbbe051808ffcc345789c3a67a52f354e
Luke Jones <luke@freerunningtechnologies.com>
2015-07-27 15:58:56 -0700

    Add slack functionality to notification
    This commit adds slack support for users that are configured for it. Otherwise
    a message is logged and the user is sent an email instead.

--- spec/notification_spec.rb
+++ spec/notification_spec.rb
@@ -7,9 +7,32 @@ RSpec.describe Notification do
   let(:sender) { User.new(email: "sender@example.com") }
   let(:message) { "arbitrary message" }
 
-  describe "#deliver" do
-    subject { notification.deliver }
+  let(:notifier_double) { double("Slack::Notifier") }
 
-    it { should have_sent_email }
+  before(:each) do
+    allow(Slack::Notifier).to receive(:new).and_return notifier_double
+  end
+
+  describe "#deliver" do
+    subject { notification.deliver("test-key") }
+
+    context "when recipient has a slack id" do
+      before(:each) do
+        allow(recipient).to receive(:slack_id).and_return "my_name"
+      end
+
+      it "sends a slack notification" do
+        expect(notifier_double).to receive(:ping)
+        subject
+      end
+    end
+
+    context "when recipient does not have a slack id" do
+      before(:each) do
+        allow(recipient).to receive(:slack_id).and_return nil
+      end
+
+      it { should have_sent_email }
+    end
   end
 end
