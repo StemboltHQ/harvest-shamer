a63b572b446acae5434f36b0443015634a8300c7
Luke Jones <luke@freerunningtechnologies.com>
2015-07-27 15:58:56 -0700

    Add slack functionality to notification
    This commit adds slack support for users that are configured for it. Otherwise
    a message is logged and the user is sent an email instead.

--- models/notification.rb
+++ models/notification.rb
@@ -7,9 +7,28 @@ class Notification
     @message = message
   end
 
-  def deliver
-    Log.print "emailing #{recipient.first_name} #{recipient.last_name}"
+  def deliver(webhook_url)
+    if recipient.slack_id
+      slack(webhook_url)
+      Log.print "notified #{recipient.slack_id}"
+    else
+      Log.print "no slack_id could be found for #{recipient.email}, emailing instead"
+      email
+    end
+  end
 
+  private
+
+  def slack(webhook_url)
+    notifier = Slack::Notifier.new(webhook_url, {
+      channel: "@#{recipient.slack_id}",
+      username: "Harvest Shamer",
+      icon_emoji: ":thumbsdown:"
+    })
+    notifier.ping message
+  end
+
+  def email
     Mail.new(
       from: sender.email,
       to: recipient.email,
